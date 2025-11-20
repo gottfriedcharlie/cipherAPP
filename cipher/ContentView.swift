8878e05b1ccfbd4535bf570c1ce4602a32c067fd
gottfriedcharlie <93043695+gottfriedcharlie@users.noreply.github.com>
2025-11-19 20:47:59 -0500

    Initial Commit

---
+++ cipher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cipher
+//
+//  Created by Charlie Gottfried on 11/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cipher/cipherApp.swift
@@ -0,0 +1,17 @@
+//
+//  cipherApp.swift
+//  cipher
+//
+//  Created by Charlie Gottfried on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct cipherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
