f4f7887c5f6b466e751b6b05313fc61228541491
gottfriedcharlie <93043695+gottfriedcharlie@users.noreply.github.com>
2025-11-24 23:34:12 -0500

    finished playfair encoder and added a quick page for it

--- cipher/Views/sheetViews/playfairSheet.swift
+++ cipher/Views/sheetViews/playfairSheet.swift
@@ -1,5 +1,5 @@
 //
-//  ceasarSheet.swift
+//  playfairSheet.swift
 //  cipher
 //
 //  Created by Charlie Gottfried on 11/22/25.
@@ -8,20 +8,87 @@
 import SwiftUI
 
 struct playfairSheet: View {
-    // This environment variable allows the sheet to dismiss itself
+    //This environment variable allows the sheet to dismiss itself
     @Environment(\.dismiss) var dismiss
-
+    @State private var key: String = " "
+    @State private var plaintext: String = " "
+    @State private var ciphertext: String = " "
+    
+    //controls ceasar sheet
+    @State private var showResultSheet = false
+    
     var body: some View {
-        VStack(spacing: 20) {
-            Text("playfairSheet")
-                .font(.title)
-            
-            Button("Close") {
-                dismiss()
+        NavigationView {
+            VStack(spacing: 15) {
+                
+                Text("Enter A keyword Below Ex: Playfair")
+                    .fontWeight(.bold)
+                
+                HStack{
+                    Spacer()
+                    TextEditor(text: $key)
+                        .frame(maxHeight: 50)
+                        .frame(maxWidth: 150)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.secondary.opacity(0.5))
+                        )
+                    Spacer()
+                }
+                
+                Text("Enter the plaintext below")
+                    .fontWeight(.bold)
+                Text("For propper encrytion it will clean the text. Example: Play: Fair -> playfair")
+                    .font(.caption)
+                
+                TextEditor(text: $plaintext)
+                    .frame(maxHeight: 350)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.secondary.opacity(0.5))
+                    )
+                    .foregroundColor(Color.gray)
+                    .font(.custom("HelveticaNeue", size: 13))
+                    .onChange(of: plaintext) { oldValue, newValue in
+                        plaintext = newValue
+                    }
+                
+                //this button updates to show result sheet
+                Button(action: {
+                    let keyWord = String(key).uppercased().filter { $0.isLetter }
+                    
+                    ciphertext = playfairEncode.encrypt(plainText: plaintext, keyWord: keyWord)
+                    
+                    
+                    
+                    
+                    showResultSheet = true
+                }) {
+                    Text("Encrpt Text")
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.blue)
+                
+                Spacer()
+                
             }
-            .buttonStyle(.bordered)
+            .navigationTitle(Text("Play Fair Cipher"))
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Close") {
+                        dismiss()
+                    }
+                }
+            }
+            .padding()
+            // 3. Attach the sheet modifier here
+            .sheet(isPresented: $showResultSheet) {
+                // This is the content of the pop-up sheet
+                resultSheet(cipherText: ciphertext, showResultSheet: $showResultSheet)
+                    .presentationDetents([.fraction(0.85)])
+            }
         }
-        .padding()
     }
 }
 
---
+++ cipher/encoders/playfairEncode.swift
@@ -0,0 +1,171 @@
+//
+//  playfairEncode.swift
+//  cipher
+//
+//  Created by Charlie Gottfried on 11/24/25.
+//
+
+import Foundation
+
+struct playfairEncode {
+    
+    static func encrypt(plainText: String, keyWord: String) -> String {
+        
+        //get rid of J because playfair doesnt like J and replaces him with I 
+        let normalizedPlain = plainText
+            .uppercased()
+            .replacingOccurrences(of: "J", with: "I")
+        
+        var trimmedText = Array(normalizedPlain.filter { $0.isLetter })
+        var cleanedText = ""
+        var plaintextCount: Int = 0
+        var dividors: Int = 0
+        
+        
+        let matrix = createMatrix(keyWord: keyWord)
+        print(matrix)
+        
+        
+        //MARK: this prepares the plaintext for encoding
+        //if text is not even add X at the end
+        if trimmedText.count.isMultiple(of: 2) == false {
+            trimmedText.append("X")
+        }
+        
+        //for multiple letters add X inbetween
+        for i in 0..<trimmedText.count {
+            cleanedText.append(trimmedText[i])
+            
+            if i < trimmedText.count - 1 && trimmedText[i] == trimmedText[i + 1] {
+                cleanedText.append("X")
+            }
+        }
+        
+        plaintextCount = Array(cleanedText).count
+        
+        //maks sure plaintexts is even so we can split into pairs
+        if !plaintextCount.isMultiple(of: 2) {
+            cleanedText.append("X")
+            plaintextCount += 1
+        }
+        
+        dividors = plaintextCount / 2
+        
+        var cipherText: [String] = []
+        let cleanedChars = Array(cleanedText)
+        
+
+        //index insures we grab pairs
+        for i in 0..<dividors {
+            let index = i * 2
+            let first = cleanedChars[index]
+            let second = cleanedChars[index + 1]
+            
+            let cipheredPair = playFairRules(char1: first, char2: second, matrix: matrix)
+            cipherText.append(cipheredPair)
+        }
+        
+        let encryptedtext = cipherText.joined()
+        
+        return encryptedtext
+    }
+    
+    
+    
+    //this fucntion take the keywords and creates a 2d array with it and
+    //the remaining character in the alpahabet hence why it returns a 2d array of characters
+    static func createMatrix(keyWord: String) -> [[Character]] {
+        let alphabet = Array("ABCDEFGHIKLMNOPQRSTUVWXYZ")
+        
+        let input = (keyWord.uppercased() + alphabet).replacingOccurrences(of: "J", with: "I")
+        
+        var uniqueChars: [Character] = []
+        
+        //does not store duplicates so a set is used
+        var seenChars: Set<Character> = []
+        
+        for char in input {
+            if char.isLetter  && !seenChars.contains(char) {
+                uniqueChars.append(char)
+                seenChars.insert(char)
+            }
+        }
+        
+        //create the 5 by 5 matrix to refer to
+        var matrix = Array(repeating: Array(repeating: Character(" "), count: 5), count: 5)
+        
+        //index is used to make sure we get all aviable characters and to select the correct
+        //character in uniqueChars
+        var index = 0
+        //rows
+        for i in 0..<5 {
+            //cols
+            for j in 0..<5 {
+                if index < uniqueChars.count {
+                    matrix[i][j] = uniqueChars[index]
+                    index += 1
+                }
+            }
+        }
+        return matrix
+    }
+    
+    //this function will ask the rules to determine how the plaintext gets ciphered up
+    //three rules
+    //rule 1 is if they are in the same row, replace with Right element loop around to start
+    //rule 2 if they are in the same column, replace with Below element loop around to top
+    //rule 3 is if they are in neither, draw a rectange in the table and take the opposite corners
+    static func playFairRules(char1: Character, char2: Character, matrix: [[Character]]) -> String {
+        
+        let char1Coordinates = findPosition(of: char1, in: matrix)
+        let char2Coordinates = findPosition(of: char2, in: matrix)
+        
+        var cipherChar1 : Character
+        var cipherChar2 : Character
+        var encodedChars: String = ""
+        //unwrapping making sure char1 and char2 are valid, stupid errors making me add this
+        if let char1Coordinates = char1Coordinates,
+           let char2Coordinates = char2Coordinates {
+            
+            
+            //rule 1
+            if char1Coordinates.row == char2Coordinates.row {
+                cipherChar1 = matrix[char1Coordinates.row][(char1Coordinates.col + 1) % 5]
+                cipherChar2 = matrix[char2Coordinates.row][(char2Coordinates.col + 1) % 5]
+            }
+            
+            //rule 2
+            else if char1Coordinates.col == char2Coordinates.col {
+                cipherChar1 = matrix[(char1Coordinates.row + 1) % 5][char1Coordinates.col]
+                cipherChar2 = matrix[(char2Coordinates.row + 1) % 5][char2Coordinates.col]
+            }
+            
+            //rule 3
+            else {
+                cipherChar1 = matrix[char1Coordinates.row][char2Coordinates.col]
+                cipherChar2 = matrix[char2Coordinates.row][char1Coordinates.col]
+            }
+            
+            
+            encodedChars.append(cipherChar1)
+            encodedChars.append(cipherChar2)
+        }
+        return encodedChars
+    }
+    
+    
+    
+    
+    //static because its just a simple helper function for this struct
+    //this function find the position of the character both row and col
+    static func findPosition(of char: Character, in matrix: [[Character]]) -> (row: Int, col: Int)? {
+        for (rowIndex, row) in matrix.enumerated() {
+            if let colIndex = row.firstIndex(of: char) {
+                return (rowIndex, colIndex)
+            }
+        }
+        return nil
+    }
+    
+    
+}
